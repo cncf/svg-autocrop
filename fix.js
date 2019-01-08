cc5940727b40c43ea7752a5b346667fb4e7e541e
ZeusTheTrueGod <l-2732@yandex.ru>
2019-01-08 18:36:03 +0000

    adding a tool to fix all svg in a given folder

---
+++ fix.js
@@ -0,0 +1,20 @@
+const _ = require('lodash');
+const autoCropSvg  = require('./index');
+
+async function main() {
+    const files = require('fs').readdirSync('images');
+    const inputFiles = files.filter( (x) => x.indexOf('.processed.') === -1 && x.indexOf('.svg') !== -1);
+    for (var file of inputFiles) {
+        const inputFile = `./images/${file}`;
+        const outputFile = `./images/${file.replace('.svg','.processed.svg')}`;
+        const inputContent = require('fs').readFileSync(inputFile, 'utf-8');
+        try {
+            const convertedSvg = await autoCropSvg(inputContent);
+            require('fs').writeFileSync(outputFile, convertedSvg);
+            console.info(`Processed ${inputFile} and saved to ${outputFile}`);
+        } catch(ex) {
+            console.info(`Failed to process ${inputFile}, error is ${ex.message.substring(0, 1000)}`);
+        }
+    }
+}
+main().catch(console.info);
