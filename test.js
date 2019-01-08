29ebd30d0f2c59d4ca2d1040b330a62f2dcd7be6
ZeusTheTrueGod <l-2732@yandex.ru>
2019-01-08 19:25:28 +0000

    adding a test with a bad input

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 const _ = require('lodash');
 const autoCropSvg  = require('./index');
+const captureMode = false;
 
 async function main() {
     const files = require('fs').readdirSync('fixtures');
@@ -7,17 +8,44 @@ async function main() {
     for (var file of inputFiles.slice(0, 5555)) {
         const inputFile = `./fixtures/${file}`;
         const outputFile = `./fixtures/${file.replace('input','output')}`;
+        const errorFile = `./fixtures/${file.replace('input','error').replace('.svg', '.txt')}`;
         const inputContent = require('fs').readFileSync(inputFile, 'utf-8');
-        const convertedSvg = await autoCropSvg(inputContent);
-        console.info(inputFile);
-        require('fs').writeFileSync(outputFile, convertedSvg);
-        // const outputContent = require('fs').readFileSync(outputFile, 'utf-8');
-        // if (convertedSvg !== outputContent) {
-            // console.info(`Fixture do not match: ${inputFile}, ${outputFile}`, convertedSvg.substring(0, 1000), outputContent.substring(0, 1000));
-            // process.exit(1);
-        // } else {
-            // console.info('Match');
-        // }
+        let convertedSvg;
+        let errorMessage;
+        try {
+            convertedSvg = await autoCropSvg(inputContent);
+        } catch (ex) {
+            const message = ex.message || ex;
+            errorMessage = message;
+        }
+
+        if (captureMode) {
+            if (convertedSvg) {
+                require('fs').writeFileSync(outputFile, convertedSvg);
+            } else {
+                require('fs').writeFileSync(errorFile, errorMessage);
+            }
+        } else {
+            if (convertedSvg) {
+                const outputContent = require('fs').readFileSync(outputFile, 'utf-8');
+                if (convertedSvg !== outputContent) {
+                    console.info(`Fixture do not match: ${inputFile}, ${outputFile}`, convertedSvg.substring(0, 1000), outputContent.substring(0, 1000));
+                    process.exit(1);
+                } else {
+                    console.info('Match');
+                }
+            } else {
+                const expectedMessage = require('fs').readFileSync(errorFile, 'utf-8');
+                console.info('Match');
+                if (expectedMessage !== errorMessage) {
+                    console.info(`Fixture do not match: on invalid ${inputFile}, error is ${errorMessage}, but we need ${expectedMessage}`);
+                    process.exit(1);
+                }
+            }
+        }
     }
 }
-main().catch(console.info);
+main().catch(function(x) {
+    console.info(x);
+    process.exit(1)
+});
