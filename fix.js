33dd7a916126bb6229957adfcaa101be3c52a76d
ZeusTheTrueGod <l-2732@yandex.ru>
2019-05-01 20:54:33 +0100

    adding a recursive mode

--- fix.js
+++ fix.js
@@ -1,15 +1,41 @@
 #!/usr/bin/env node
 const _ = require('lodash');
+const path = require('path');
 const autoCropSvg  = require('./index');
 
+function getInputFiles() {
+    if (process.argv[2] === '--recursive') {
+        console.info('Recursive change');
+        const dir = process.argv[3] || process.cwd();
+        var glob = require("glob");
+        const files = glob.sync('**/*.svg', {cwd: dir });
+        return {
+            inputFiles: files.map( (x) => path.resolve(dir, x)),
+            inputFn: (file) => file,
+            outputFn: (file) => file
+        }
+    } else {
+        const files = require('fs').readdirSync('input');
+        const inputFiles = files.filter( (x) => x.indexOf('.processed.') === -1 && x.indexOf('.svg') !== -1);
+        return {
+            inputFiles,
+            inputFn: (file) => `./input/${file}`,
+            outputFn: (file) => `./output/${file}`
+        }
+    }
+}
+
 async function main() {
-    const files = require('fs').readdirSync('input');
-    const inputFiles = files.filter( (x) => x.indexOf('.processed.') === -1 && x.indexOf('.svg') !== -1);
+    const inputFileOptions = getInputFiles();
+    const inputFiles = inputFileOptions.inputFiles;
+    const inputFn = inputFileOptions.inputFn;
+    const outputFn = inputFileOptions.outputFn;
     for (var file of inputFiles) {
-        const inputFile = `./input/${file}`;
-        const outputFile = `./output/${file}`;
+        const inputFile = inputFn(file);
+        const outputFile = outputFn(file);
         const inputContent = require('fs').readFileSync(inputFile, 'utf-8');
         try {
+            console.info(`Processing ${inputFile} and saving to ${outputFile}`);
             const convertedSvg = await autoCropSvg(inputContent);
             require('fs').writeFileSync(outputFile, convertedSvg);
             console.info(`Processed ${inputFile} and saved to ${outputFile}`);
