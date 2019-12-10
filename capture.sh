76ee3e4b9db252324caf7703eb1c7cb911857624
ZeusTheTrueGod <l-2732@yandex.ru>
2019-12-10 18:15:45 +0000

    using a docker image from circleci

--- capture.sh
+++ capture.sh
@@ -1,1 +1,1 @@
-docker run -it --rm -v $(pwd):/app -v $(pwd)/docker_node_modules:/app/node_modules buildkite/puppeteer bash -lc 'cd /app;npm install; CAPTURE=1 npm run test'
+docker run -it --rm -v $(pwd):/app -v $(pwd)/docker_node_modules:/app/node_modules circleci/node:current-stretch-browsers-legacy bash -lc 'cd /app;npm install; CAPTURE=1 npm run test'
