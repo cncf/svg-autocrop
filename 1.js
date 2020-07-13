cb3c18a644e5346316a38ea854336b10b8c3c342
Andrey Kozlov <l-2732@yandex.ru>
2020-07-13 21:39:32 +0100

    upgrade to yarn

--- 1.js
+++
@@ -1,8 +0,0 @@
-const addText = require('./addText');
-
-const newSvg = addText({
-    color: 'blue',
-    text: 'Hi there!',
-    input: require('fs').readFileSync('fixtures/ambassador.input.svg', 'utf-8')
-});
-require('fs').writeFileSync('/tmp/1.svg', newSvg);
--- 2.js
+++
@@ -1,8 +0,0 @@
-const autoCropSvg  = require('./index');
-
-async function main() {
-    const input = require('fs').readFileSync('fixtures/white.input.svg', 'utf-8');
-    const result = await autoCropSvg(input, {title: `logo`, caption: 'Hello, world! forever', captionWidth: 0.9});
-    require('fs').writeFileSync('/tmp/1.svg', result.result);
-}
-main();
